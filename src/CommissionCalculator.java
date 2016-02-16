5c32de1dfcd627e64b9459022d40c4ee61d2f0f8
Phillip Bussart <phillip.bussart@gmail.com>
2016-02-16 14:37:31 -0700

    Classes seperated.

--- src/CommissionCalculator.java
+++ src/CommissionCalculator.java
@@ -1,5 +1,4 @@
 //import scanner for user input
-import java.util.Scanner;
 import java.util.ArrayList;
 
 public class CommissionCalculator {
@@ -63,56 +62,3 @@ public class CommissionCalculator {
     }
 }
 
-
-//_______________________________________________________________________
-
-
-class SalesPerson {
-    private double salary, annualSales;
-    private String name;
-    
-    public SalesPerson() { //Constructor
-        salary = 30000;
-        build();
-    }
-    //Getters and Setters
-    public void setAnnualSales(double tempD) {
-        annualSales = tempD; // Accepts input
-    }
-
-    public double getAnnualSales() {
-        return annualSales;
-    }
-    
-    public void setName(String tempS) {
-        name = tempS;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-//Working Methods//*******************************************************************
-    public void build() {
-        System.out.println("Building Salesperson...\n");
-        Scanner sc = new Scanner(System.in ); // Creates scanner object
-        
-        System.out.print("Enter the sales person's name:");        
-        String tempS = sc.nextLine();        
-        setName(tempS);
-
-        
-        System.out.print("Enter " + name + "'s annual sales amount: $");
-        double tempD = sc.nextDouble();
-        setAnnualSales(tempD);
-        sc.close();
-
-    }
-
-    public void outputAll() {
-        System.out.println("Sales person:" + "\t" + name);
-        System.out.println("Salary:" + "\t\t" + "$" + salary);
-        System.out.println("Annual sales:" + "\t" + "$" + annualSales);
-    }
-
-}
---
+++ src/SalesPerson.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+//_______________________________________________________________________
+
+
+public class SalesPerson {
+    private double salary, annualSales;
+    private String name;
+    
+    public SalesPerson() { //Constructor
+        salary = 30000;
+        build();
+    }
+    //Getters and Setters
+    public void setAnnualSales(double tempD) {
+        annualSales = tempD; // Accepts input
+    }
+
+    public double getAnnualSales() {
+        return annualSales;
+    }
+    
+    public void setName(String tempS) {
+        name = tempS;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+//Working Methods//*******************************************************************
+    public void build() {
+        System.out.println("Building Salesperson...\n");
+        Scanner sc = new Scanner(System.in ); // Creates scanner object
+        
+        System.out.print("Enter the sales person's name:");        
+        String tempS = sc.nextLine();        
+        setName(tempS);
+
+        
+        System.out.print("Enter " + name + "'s annual sales amount: $");
+        double tempD = sc.nextDouble();
+        setAnnualSales(tempD);
+        sc.close();
+
+    }
+
+    public void outputAll() {
+        System.out.println("Sales person:" + "\t" + name);
+        System.out.println("Salary:" + "\t\t" + "$" + salary);
+        System.out.println("Annual sales:" + "\t" + "$" + annualSales);
+    }
+
+}
